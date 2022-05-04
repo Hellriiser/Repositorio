e9797aebe70bc808bfc24f87e4a09b03fc39def2
Paulo Felipe <pimp-paulo@hotmail.com>
2022-05-04 15:16:59 -0300

    Adicionando pasta local

---
+++ Novo Documento de Texto.php
@@ -0,0 +1,18 @@
+<html>
+
+      <head>
+	       <title>Meu primeiro Site em PHP</title>
+	  </head>
+	  
+	  
+	  <body>
+	  
+	       <?php
+		      for ($i = 0 ; $i <10 ; $i++ ) {
+				  print( "Linha Número  " . $i . "<br />");
+			  }
+			?>
+
+		</body>
+
+</html>		
